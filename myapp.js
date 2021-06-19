40d0caa06d4c5b28510b486a7b5a5143e463b42c
unknown <jayantjadhao8@gmail.com>
2021-06-19 12:00:52 +0530

    remo cleaning

--- myapp.js
+++
@@ -1,8 +0,0 @@
-var http = require("http")
-function myfunc(request, response){
-    console.log("My app got started")
-    response.writeHead(200,{"content-Types":"text/plain"})
-    response.write("Hello Jayant")
-    response.end()
-}
-http.createServer(myfunc).listen(8888)
